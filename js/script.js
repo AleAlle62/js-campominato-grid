f038a747ca0e4022f8100629691ee53150e868a6
AleAlle62 <basketallegrini@gmail.com>
2022-11-02 17:01:13 +0100

    compleato, no bonus

--- js/script.js
+++ js/script.js
@@ -4,18 +4,19 @@ const eleGrid = document.querySelector('.grid');
 // ciclo per tutti i quadrati
 for (let i = 0; i < 100; i++) {
 
-    // stampa dei numeri 
-    eleGrid.innerHTML += `<div class=".grid">${[i]}</div>`
-
+    
     // creazione elemento HTML per le celle 
 	const eleCell = document.createElement('div');
-
+    
     // creazione di un elemento nuovo
 	eleCell.classList.add('cell');
-
+    
     // mettere un contenuto all'interno di un elemento
 	eleGrid.append(eleCell);
-
+    
+    // stampa dei numeri 
+    eleCell.innerHTML += `<div class=".grid">${[i]}</div>`
+    
     // funzione per cui se cliccli si colora collegato a CSS
 	eleCell.addEventListener('click', function () {
 		this.classList.toggle('active')
