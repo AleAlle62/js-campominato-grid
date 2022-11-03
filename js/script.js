359c880c49b63ea7115c348123333ebcd7816ffe
AleAlle62 <basketallegrini@gmail.com>
2022-11-03 13:10:16 +0100

    bonus?

--- js/script.js
+++ js/script.js
@@ -1,23 +1,22 @@
 // creazione cella classe
 const eleGrid = document.querySelector('.grid');
-let uno = document.querySelector('.uno')
-let due = document.querySelector('.due')
-let tre = document.querySelector('.tre')
-
+let uno = document.querySelector('.sceltauno')
+let due = document.querySelector('.sceltadue')
+let tre = document.querySelector('.sceltatre')
+let elePlay = document.querySelector('.btn-play')
+let livelliDifficolta = document.getElementById('livelli')
 // per il bonus, creare che ad ogni bottone corrispinde un ciclo diverso che fa scorrere i diversi for a seconda di cio che si clicca 
 
-//DIFFICOLTA 1
+//bottone play 
+elePlay.addEventListener('click', function(){
 
-
-uno.addEventListener('click', function () {
-
-    // ciclo per tutti i quadrati
-    for (let i = 0; i < 100; i++) {
+      // ciclo per tutti i quadrati
+    for (let i = 1; i <= 100; i++) {
             
         // creazione elemento HTML per le celle 
         const eleCell = document.createElement('div');
         
-        // creazione di un elemento nuovo
+        // aggiunta all'elemento nuovo
         eleCell.classList.add('cell');
             
         // mettere un contenuto all'interno di un elemento
@@ -32,64 +31,93 @@ uno.addEventListener('click', function () {
         });
     }
     
-});
+})
 
 
+// // DIFFICOLTA 1
 
+// uno.addEventListener('click', function () {
 
-//DIFFICOLTA 2
-
-
-due.addEventListener('click', function () {
-
-    // ciclo per tutti i quadrati
-    for (let i = 0; i < 81; i++) {
+//     // ciclo per tutti i quadrati
+//     for (let i = 1; i < 101; i++) {
             
-        // creazione elemento HTML per le celle 
-        const eleCell = document.createElement('div');
+//         // creazione elemento HTML per le celle 
+//         const eleCell = document.createElement('div');
         
-        // creazione di un elemento nuovo
-        eleCell.classList.add('cell');
+//         // creazione di un elemento nuovo
+//         eleCell.classList.add('cell');
             
-        // mettere un contenuto all'interno di un elemento
-        eleGrid.append(eleCell);
+//         // mettere un contenuto all'interno di un elemento
+//         eleGrid.append(eleCell);
             
-        // stampa dei numeri 
-        eleCell.innerHTML += `<div class=".grid">${[i]}</div>`
+//         // stampa dei numeri 
+//         eleCell.innerHTML += `<div class=".grid">${[i]}</div>`
         
-        // funzione per cui se cliccli si colora collegato a CSS
-        eleCell.addEventListener('click', function () {
-            this.classList.toggle('active')
-        });
-    }
-});
+//         // funzione per cui se cliccli si colora collegato a CSS
+//         eleCell.addEventListener('click', function () {
+//             this.classList.toggle('active')
+//         });
+//     }
+    
+// });
 
 
-//DIFFICOLTA 3
 
-tre.addEventListener('click', function () {
 
-        // ciclo per tutti i quadrati
-    for (let i = 0; i < 49; i++) {
+// //DIFFICOLTA 2
+
+// due.addEventListener('click', function () {
+
+//     // ciclo per tutti i quadrati
+//     for (let i = 1; i < 81; i++) {
             
-        // creazione elemento HTML per le celle 
-        const eleCell = document.createElement('div');
-            
-        // creazione di un elemento nuovo
-        eleCell.classList.add('cell');
+//         // creazione elemento HTML per le celle 
+//         const eleCell = document.createElement('div');
         
-        // mettere un contenuto all'interno di un elemento
-        eleGrid.append(eleCell);
+//         // creazione di un elemento nuovo
+//         eleCell.classList.add('cell');
             
-        // stampa dei numeri 
-        eleCell.innerHTML += `<div class=".grid">${[i]}</div>`
+//         // mettere un contenuto all'interno di un elemento
+//         eleGrid.append(eleCell);
+            
+//         // stampa dei numeri 
+//         eleCell.innerHTML += `<div class=".grid">${[i]}</div>`
         
-        // funzione per cui se cliccli si colora collegato a CSS
-        eleCell.addEventListener('click', function () {
-            this.classList.toggle('active')
-        });
-    }
-});
+//         // funzione per cui se cliccli si colora collegato a CSS
+//         eleCell.addEventListener('click', function () {
+//             this.classList.toggle('active')
+//         });
+//     }
+// });
+
+
+
+
+// //DIFFICOLTA 3
+
+// tre.addEventListener('click', function () {
+
+//         // ciclo per tutti i quadrati
+//     for (let i = 1; i < 49; i++) {
+            
+//         // creazione elemento HTML per le celle 
+//         const eleCell = document.createElement('div');
+            
+//         // creazione di un elemento nuovo
+//         eleCell.classList.add('cell');
+        
+//         // mettere un contenuto all'interno di un elemento
+//         eleGrid.append(eleCell);
+            
+//         // stampa dei numeri 
+//         eleCell.innerHTML += `<div class=".grid">${[i]}</div>`
+        
+//         // funzione per cui se cliccli si colora collegato a CSS
+//         eleCell.addEventListener('click', function () {
+//             this.classList.toggle('active')
+//         });
+//     }
+// });
 
 
 
